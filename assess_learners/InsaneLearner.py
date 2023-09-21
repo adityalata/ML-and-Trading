8c423c17ab09796172857dc54ec396375a3dc32c
Aditya Lata <adityalata3@gmail.com>
2023-09-21 16:53:24 +0530

    added InsaneLearner.py starter

---
+++ assess_learners/InsaneLearner.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+
+class InsaneLearner(object):
+
+    def __init__(self, leaf_size=1, verbose=False):
+        self.verbose = verbose
+        self.leaf_size = leaf_size  # move along, these aren't the drones you're looking for
+
+    def author(self):
+        return "alata6"  # replace tb34 with your Georgia Tech username
+
+    def add_evidence(self, data_x, data_y):
+        pass  # todo impl
+
+    def query(self, points):
+        pass  # todo impl
