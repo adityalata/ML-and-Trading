ee7d1878cb9dbc6f1f5addc4ad6e5974681a3c8d
Aditya Lata <adityalata3@gmail.com>
2023-09-21 16:47:19 +0530

    added RTLearner.py starter

---
+++ assess_learners/RTLearner.py
@@ -0,0 +1,51 @@
+import numpy as np
+
+
+class RTLearner(object):
+    """
+    This is a regression Random Tree Learner.
+
+    :param verbose: If “verbose” is True, your code can print out information for debugging.
+        If verbose = False your code should not generate ANY output. When we test your code, verbose will be False.
+    :type verbose: bool
+    """
+
+    def __init__(self, leaf_size=1, verbose=False):
+        """
+        Constructor method
+        """
+        self.verbose = verbose
+        self.leaf_size = leaf_size  # move along, these aren't the drones you're looking for
+
+    def author(self):
+        """
+        :return: The GT username of the student
+        :rtype: str
+        """
+        return "alata6"  # replace tb34 with your Georgia Tech username
+
+    def add_evidence(self, data_x, data_y):
+        """
+        Add training data to learner
+
+        :param data_x: A set of feature values used to train the learner
+        :type data_x: numpy.ndarray
+        :param data_y: The value we are attempting to predict given the X data
+        :type data_y: numpy.ndarray
+        """
+    #     todo impl
+
+    def query(self, points):
+        """
+        Estimate a set of test points given the model we built.
+
+        :param points: A numpy array with each row corresponding to a specific query.
+        :type points: numpy.ndarray
+        :return: The predicted result of the input data according to the trained model
+        :rtype: numpy.ndarray
+        """
+        #         todo impl
+
+
+if __name__ == "__main__":
+    print("the secret clue is 'zzyzx'")
