61db8462f6f76ae0f87ae82024176cee4cd548bb
Aditya Lata <alata6@gatech.edu>
2023-09-24 03:39:20 +0530

    adding exp3 fig

--- assess_learners/testlearner.py
+++ assess_learners/testlearner.py
@@ -89,7 +89,7 @@ if __name__ == "__main__":
   		  	   		  		 		  		  		    	 		 		   		 		  
     print("test_x.shape", f"{test_x.shape}")
     print("test_y.shape", f"{test_y.shape}")
-    verbose = False  # todo make False before submission
+    verbose = False
   		  	   		  		 		  		  		    	 		 		   		 		  
     # create a learner and train it
     print("====================================================================")
@@ -239,7 +239,7 @@ if __name__ == "__main__":
     print("====================================================================")
 
     print('Starting Experiment 1')
-    exp1_max_leaf_size = 50
+    exp1_max_leaf_size = 30
     exp1_array_size = exp1_max_leaf_size+1
     exp1_rmse_in_sample = np.zeros(exp1_array_size)
     exp1_rmse_out_of_sample = np.zeros(exp1_array_size)
@@ -284,7 +284,7 @@ if __name__ == "__main__":
     print("====================================================================")
 
     print('Starting Experiment 2')
-    exp2_max_leaf_size = 50
+    exp2_max_leaf_size = 30
     exp2_bag_size = 20
     exp2_array_size = exp2_max_leaf_size + 1
     exp2_rmse_in_sample = np.zeros(exp2_array_size)
@@ -316,7 +316,7 @@ if __name__ == "__main__":
           exp2_min_rmse_out_leaf_size)
 
     plt.figure(2)
-    plt.axis([0, exp2_max_leaf_size, 0, max(exp2_max_rmse_in_sample, exp2_max_rmse_out_sample)])
+    plt.axis([0, exp2_max_leaf_size, 0, max(exp2_max_rmse_in_sample, exp2_max_rmse_out_sample, exp1_max_rmse_in_sample, exp1_max_rmse_out_sample)])
     plt.axvline(x=exp2_min_rmse_out_leaf_size, color='r', label='BagL - Overfitting leaf size', linestyle='dashed')
     plt.xlabel('Leaf Size')
     plt.xticks(ticks=exp2_xticks, rotation=45)
@@ -327,7 +327,63 @@ if __name__ == "__main__":
     plt.plot(exp2_rmse_out_of_sample, label='BagL - Out of Sample Test')
     plt.plot(exp1_rmse_in_sample, label='DT - In Sample Test')
     plt.plot(exp1_rmse_out_of_sample, label='DT - Out of Sample Test')
+    plt.axvline(x=exp1_min_rmse_out_leaf_size, color='b', label='DT - Overfitting leaf size', linestyle='dashed')
 
     plt.legend(loc='lower right', shadow=True, fontsize='medium')
     plt.savefig('Experiment_2.png')
     print("====================================================================")
+    
+    print('Starting Experiment 3')
+    exp3_max_leaf_size = 30
+    exp3_array_size = exp3_max_leaf_size+1
+    exp3_dt_rmse_in_sample = np.zeros(exp3_array_size)
+    exp3_dt_rmse_out_of_sample = np.zeros(exp3_array_size)
+    exp3_rt_rmse_in_sample = np.zeros(exp3_array_size)
+    exp3_rt_rmse_out_of_sample = np.zeros(exp3_array_size)
+    exp3_xticks = []
+
+    for i in range(exp3_array_size):
+        exp3_dt_trail_learner = dt.DTLearner(leaf_size=i, verbose=verbose)
+        exp3_dt_trail_learner.add_evidence(train_x, train_y)
+        exp3_rt_trail_learner = rt.RTLearner(leaf_size=i, verbose=verbose)
+        exp3_rt_trail_learner.add_evidence(train_x, train_y)
+        if i % 2:
+            exp3_xticks.append(i)
+
+        # in sample - train exp
+        exp3_dt_trail_pred_train_y = exp3_dt_trail_learner.query(train_x)
+        exp3_dt_rmse_in_sample[i] = root_mean_squared_error(train_y, exp3_dt_trail_pred_train_y)
+        exp3_rt_trail_pred_train_y = exp3_rt_trail_learner.query(train_x)
+        exp3_rt_rmse_in_sample[i] = root_mean_squared_error(train_y, exp3_rt_trail_pred_train_y)
+
+        # out of sample - test exp
+        exp3_dt_trail_pred_test_y = exp3_dt_trail_learner.query(test_x)
+        exp3_dt_rmse_out_of_sample[i] = root_mean_squared_error(exp3_dt_trail_pred_test_y, test_y)
+        exp3_rt_trail_pred_test_y = exp3_rt_trail_learner.query(test_x)
+        exp3_rt_rmse_out_of_sample[i] = root_mean_squared_error(exp3_rt_trail_pred_test_y, test_y)
+
+    exp3_dt_max_rmse_in_sample = np.nanmax(exp3_dt_rmse_in_sample)
+    exp3_dt_max_rmse_out_sample = np.nanmax(exp3_dt_rmse_out_of_sample)
+    exp3_dt_min_rmse_out_sample = np.nanmin(exp3_dt_rmse_out_of_sample)
+    exp3_dt_min_rmse_out_leaf_size = np.argsort(exp3_dt_rmse_out_of_sample)[0]
+    exp3_rt_max_rmse_in_sample = np.nanmax(exp3_rt_rmse_in_sample)
+    exp3_rt_max_rmse_out_sample = np.nanmax(exp3_rt_rmse_out_of_sample)
+    # print("exp3_dt_rmse_in_sample ", exp3_dt_rmse_in_sample)
+    # print("exp3_dt_rmse_out_of_sample ", exp3_dt_rmse_out_of_sample)
+    print('exp3_dt_min_rmse_out_sample ', exp3_dt_min_rmse_out_sample, " exp3_dt_max_rmse_in_sample ", exp3_dt_max_rmse_in_sample, " exp3_dt_max_rmse_out_sample ", exp3_dt_max_rmse_out_sample, "exp3_dt_min_rmse_out_leaf_size", exp3_dt_min_rmse_out_leaf_size)
+
+    plt.figure(3)
+    plt.axis([0, exp3_max_leaf_size, 0, max(exp3_dt_max_rmse_in_sample, exp3_dt_max_rmse_out_sample, exp3_rt_max_rmse_in_sample, exp3_rt_max_rmse_out_sample)])
+    plt.axvline(x=exp3_dt_min_rmse_out_leaf_size, color='r', label='DT Overfitting leaf size', linestyle='dashed')
+    plt.xlabel('Leaf Size')
+    plt.xticks(ticks=exp3_xticks, rotation=45)
+    plt.ylabel('Root Mean Squared Error (RMSE)')
+    plt.title('Figure 3: DT vs RT Learner - alata6')
+
+    plt.plot(exp3_dt_rmse_in_sample, label='DT - In Sample Test')
+    plt.plot(exp3_dt_rmse_out_of_sample, label='DT - Out of Sample Test')
+    plt.plot(exp3_rt_rmse_in_sample, label='RT - In Sample Test')
+    plt.plot(exp3_rt_rmse_out_of_sample, label='RT - Out of Sample Test')
+
+    plt.legend(loc='lower right', shadow=True, fontsize='medium')
+    plt.savefig('Experiment_3.png')
