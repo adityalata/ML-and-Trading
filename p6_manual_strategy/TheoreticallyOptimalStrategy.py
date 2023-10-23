85a90d1dd1bdd4bfa055b3951f95b5320cea3bc3
Aditya Lata <alata6@gatech.edu>
2023-10-23 14:23:57 +0530

    refactor

--- p6_manual_strategy/TheoreticallyOptimalStrategy.py
+++ p6_manual_strategy/TheoreticallyOptimalStrategy.py
@@ -1,15 +1,11 @@
-"""
-    Theoretically Optimal Strategy
-"""
-
-import math
 import datetime as dt
-import pandas as pd
-import numpy as np
+import math
 import matplotlib.pyplot as plt
-
-from util import get_data, plot_data
+import numpy as np
+import pandas as pd
 from marketsimcode import compute_portvals
+from util import get_data
+
 
 #
 class TheoreticallyOptimalStrategy(object):
@@ -19,7 +15,7 @@ class TheoreticallyOptimalStrategy(object):
         pass
 
     #
-    def test_policy(self, symbol='AAPL', sd=dt.datetime(2010,1,1), ed=dt.datetime(2011,12,31), sv=100000):
+    def test_policy(self, symbol='AAPL', sd=dt.datetime(2010, 1, 1), ed=dt.datetime(2011, 12, 31), sv=100000):
         """
             Provides a trading strategy based on looking into the future
         """
@@ -32,43 +28,44 @@ class TheoreticallyOptimalStrategy(object):
         tos_orders['Order'] = np.nan
         tos_orders['Shares'] = np.nan
 
-        tos_orders.rename(columns={symbol:'Symbol'}, inplace=True)
+        tos_orders.rename(columns={symbol: 'Symbol'}, inplace=True)
 
-        net_holdings = 0 # 0 shares, 1000 shares, or -1000 shares
+        net_holdings = 0  # 0 shares, 1000 shares, or -1000 shares
 
-        for index in range(tos_orders.shape[0] - 1): # iterate over dataframe
+        for index in range(tos_orders.shape[0] - 1):  # iterate over dataframe
             curr_date = tos_orders.index[index]
             next_date = tos_orders.index[index + 1]
 
             curr_price = prices.at[curr_date, symbol]
             next_price = prices.at[next_date, symbol]
 
-            if curr_price < next_price: # price goes up tomorrow -> BUY shares now
+            if curr_price < next_price:  # price goes up tomorrow -> BUY shares now
 
-                if net_holdings == 0: # can buy at most 1000 shares
-                    tos_orders.loc[curr_date] = pd.Series({'Symbol':symbol, 'Order':'BUY', 'Shares':1000})
+                if net_holdings == 0:  # can buy at most 1000 shares
+                    tos_orders.loc[curr_date] = pd.Series({'Symbol': symbol, 'Order': 'BUY', 'Shares': 1000})
                     net_holdings += 1000
 
-                elif net_holdings == -1000: # can buy at most 2000 shares
-                    tos_orders.loc[curr_date] = pd.Series({'Symbol':symbol, 'Order':'BUY', 'Shares':2000})
+                elif net_holdings == -1000:  # can buy at most 2000 shares
+                    tos_orders.loc[curr_date] = pd.Series({'Symbol': symbol, 'Order': 'BUY', 'Shares': 2000})
                     net_holdings += 2000
 
-            elif curr_price > next_price: # price goes down tomorrow -> SELL/SHORT shares now
+            elif curr_price > next_price:  # price goes down tomorrow -> SELL/SHORT shares now
 
-                if net_holdings == 0: # can sell at most 1000 shares
-                    tos_orders.loc[curr_date] = pd.Series({'Symbol':symbol, 'Order':'SELL', 'Shares':1000})
+                if net_holdings == 0:  # can sell at most 1000 shares
+                    tos_orders.loc[curr_date] = pd.Series({'Symbol': symbol, 'Order': 'SELL', 'Shares': 1000})
                     net_holdings -= 1000
 
-                elif net_holdings == 1000: # can sell at most 2000 shares
-                    tos_orders.loc[curr_date] = pd.Series({'Symbol':symbol, 'Order':'SELL', 'Shares':2000})
+                elif net_holdings == 1000:  # can sell at most 2000 shares
+                    tos_orders.loc[curr_date] = pd.Series({'Symbol': symbol, 'Order': 'SELL', 'Shares': 2000})
                     net_holdings -= 2000
 
-        tos_orders.fillna(value=pd.Series({'Symbol':symbol, 'Order':'BUY', 'Shares':0}), axis=0, inplace=True) # replace NaNs with empty BUY orders
+        tos_orders.fillna(value=pd.Series({'Symbol': symbol, 'Order': 'BUY', 'Shares': 0}), axis=0,
+                          inplace=True)  # replace NaNs with empty BUY orders
 
         return tos_orders
 
     #
-    def base_policy(self, symbol='AAPL', sd=dt.datetime(2010,1,1), ed=dt.datetime(2011,12,31), sv=100000):
+    def base_policy(self, symbol='AAPL', sd=dt.datetime(2010, 1, 1), ed=dt.datetime(2011, 12, 31), sv=100000):
         """
             Implements a basic trading strategy of investing 1000 shares in a stock and holding
             that position for the duration of the time interval
@@ -83,7 +80,7 @@ class TheoreticallyOptimalStrategy(object):
         benchmark_orders['Order'] = 'BUY'
         benchmark_orders['Shares'] = 0
 
-        benchmark_orders.rename(columns={symbol:'Symbol'}, inplace=True)
+        benchmark_orders.rename(columns={symbol: 'Symbol'}, inplace=True)
         benchmark_orders.at[actual_start, 'Shares'] = 1000
 
         return benchmark_orders
@@ -94,11 +91,12 @@ class TheoreticallyOptimalStrategy(object):
             Compares the Theoretically Optimal Strategy approach to the benchmark approach
         """
 
-        sd = dt.datetime(2008,1,1)
-        ed = dt.datetime(2009,12,31)
+        sd = dt.datetime(2008, 1, 1)
+        ed = dt.datetime(2009, 12, 31)
 
         benchmark_orders = self.base_policy(symbol='JPM', sd=sd, ed=ed)
-        benchmark_portval = compute_portvals(benchmark_orders, 100000, 0.0, 0.0) # ignore commission and market impact for theoretically optimal strategy
+        benchmark_portval = compute_portvals(benchmark_orders, 100000, 0.0,
+                                             0.0)  # ignore commission and market impact for theoretically optimal strategy
         benchmark_portval = benchmark_portval.to_frame(name='Benchmark PortVal')
         benchmark_portval /= benchmark_portval.iloc[0]
 
@@ -109,18 +107,21 @@ class TheoreticallyOptimalStrategy(object):
 
         portval_df = pd.concat([benchmark_portval, tos_portval], axis=1)
 
-        portval_graph = portval_df.plot(title="Benchmark & Theoretically Optimal Portfolio Value - alata6", fontsize=12, grid=True, color=['blue', 'black'])
+        portval_graph = portval_df.plot(title="Benchmark & Theoretically Optimal Portfolio Value - alata6", fontsize=12,
+                                        grid=True, color=['blue', 'black'])
         portval_graph.set_xlabel('Date')
         portval_graph.set_ylabel('Normalized Portfolio Value ($)')
 
         plt.savefig('Figure_4.png')
 
-        benchmark_cr = (benchmark_portval.iloc[-1].at['Benchmark PortVal'] / benchmark_portval.iloc[0].at['Benchmark PortVal']) - 1
+        benchmark_cr = (benchmark_portval.iloc[-1].at['Benchmark PortVal'] / benchmark_portval.iloc[0].at[
+            'Benchmark PortVal']) - 1
         benchmark_adr = benchmark_portval.pct_change(1).mean()['Benchmark PortVal']
         benchmark_sddr = benchmark_portval.pct_change(1).std()['Benchmark PortVal']
         benchmark_sr = math.sqrt(252.0) * (benchmark_adr / benchmark_sddr)
 
-        tos_cr = (tos_portval.iloc[-1].at['Theoretically Optimal Strategy PortVal'] / tos_portval.iloc[0].at['Theoretically Optimal Strategy PortVal']) - 1
+        tos_cr = (tos_portval.iloc[-1].at['Theoretically Optimal Strategy PortVal'] / tos_portval.iloc[0].at[
+            'Theoretically Optimal Strategy PortVal']) - 1
         tos_adr = tos_portval.pct_change(1).mean()['Theoretically Optimal Strategy PortVal']
         tos_sddr = tos_portval.pct_change(1).std()['Theoretically Optimal Strategy PortVal']
         tos_sr = math.sqrt(252.0) * (tos_adr / tos_sddr)
